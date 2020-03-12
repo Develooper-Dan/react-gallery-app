b4a2b7335fe593d5dbb03590851f9ff45efa9309
Daniel Grigo <daniel.grigo@freenet.de>
2020-03-12 21:34:44 +0100

    Update Gallery.js

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
 import NoResults from "./NoResults"
-import {useParams} from "react-router-dom";
+import Image from "./Image"
 
 class Gallery extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     photos: []
-  //   }
-  // }
+
 componentDidMount(){
-this.props.handleSearch(this.props.params);
+  this.props.handleSearch(this.props.routeProps.params.query)
+}
+
+componentDidUpdate(prevProps){
+  if(this.props.routeProps.params.query !== prevProps.routeProps.params.query)
+  this.props.handleSearch(this.props.routeProps.params.query)
+}
+
+createGallery(photos){
+  return photos.map(photo =>
+    <Image key= {photo.id} params= {photo}/>
+  )
 }
 
 render(){
-  console.log(this.props.params)
+    const photos = this.props.photos;
+    if(photos.length === 0){
+      return <h2>Loading...</h2>
+    }
+console.log(this.props.routeProps.params.query)
 return(
       <div className="photo-container">
-        <h2>Results</h2>
+        <h2>Results for {this.props.routeProps.params.query}</h2>
         <ul>
-          <li>
-            <img src="https://farm5.staticflickr.com/4334/37032996241_4c16a9b530.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4342/36338751244_316b6ee54b.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4343/37175099045_0d3a249629.jpg" alt="" />
-          </li>
-          <li>
-            <img src="https://farm5.staticflickr.com/4425/36337012384_ba3365621e.jpg" alt="" />
-          </li>
+          {this.createGallery(this.props.photos)}
         </ul>
         <NoResults />
       </div>
-)}
+    )}
 }
 
 export default Gallery
