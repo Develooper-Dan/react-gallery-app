613184187286f0d75190ba76f9d0c254ee085c0b
Daniel Grigo <daniel.grigo@freenet.de>
2020-03-12 21:34:37 +0100

    Create Image.js

---
+++ src/Image.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Image = (props) =>
+
+<li>
+  <img src={`https://farm${props.params.farm}.staticflickr.com/${props.params.server}/${props.params.id}_${props.params.secret}.jpg`} alt="" />
+</li>
+
+export default Image
