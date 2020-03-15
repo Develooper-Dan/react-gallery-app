d60170cde21eb6da2498b62b6fbb04b141d819bf
Daniel Grigo <daniel.grigo@freenet.de>
2020-03-15 21:16:30 +0100

    Update Image.js

--- src/Image.js
+++ src/Image.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 
 const Image = (props) =>
-
 <li>
   <img src={`https://farm${props.params.farm}.staticflickr.com/${props.params.server}/${props.params.id}_${props.params.secret}.jpg`} alt="" />
 </li>
